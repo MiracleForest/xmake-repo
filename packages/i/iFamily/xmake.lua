ec528f436ad1d43e008a796e2722d76ddac59e66
Lovelylavender4 <MiracleForest@Outlook.com>
2024-02-26 16:32:43 +0800

    repo: update iFamily.

--- packages/i/iFamily/xmake.lua
+++ packages/i/iFamily/xmake.lua
@@ -1,8 +1,8 @@
 package("iFamily")
     set_description("The iFamily package")
-
     add_urls("https://github.com/MiracleForest/iFamily.git")
-    
+    add_versions("0.1.0", "a7afe08027be02342f5062113372b6462e4c239a")
+
     on_install(function (package)
-        os.cp("iFamily/include/*.h", package:installdir("include"))
+        os.cp("include/*.h", package:installdir("include"))
     end)
