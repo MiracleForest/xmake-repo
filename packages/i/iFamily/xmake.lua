bb396ac8a896388b3a6ef66a09ff8bc23cf4b61c
Lovelylavender4 <MiracleForest@Outlook.com>
2024-02-27 13:22:04 +0800

    fix: fix iFamily.

--- packages/i/iFamily/xmake.lua
+++ packages/i/iFamily/xmake.lua
@@ -4,5 +4,5 @@ package("iFamily")
     add_versions("0.1.0", "a7afe08027be02342f5062113372b6462e4c239a")
 
     on_install(function (package)
-        os.cp("include/*.h", package:installdir("include"))
+        os.cp("iFamily/include/*.h", package:installdir("include"))
     end)
