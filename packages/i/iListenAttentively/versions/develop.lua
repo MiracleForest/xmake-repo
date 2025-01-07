304ed288205305466cf5c9f699890f5ce2a46f62
子沐呀 <163636894+zimuya4153@users.noreply.github.com>
2025-01-07 17:50:11 +0800

    fix: fix dependencies

--- packages/i/iListenAttentively/versions/develop.lua
+++ packages/i/iListenAttentively/versions/develop.lua
@@ -1,10 +1,6 @@
 local deps = {
-    "fmt 10.2.1",
     "magic_enum v0.9.5",
-    "nlohmann_json v3.11.3",
-    "levilamina 1.0.0-rc.2",
-    "levibuildscript 0.2.0",
-    "prelink 0.3.0"
+    "levilamina 1.0.0-rc.2"
 }
 
 function load(package)
