5a52df15235d67f9c6d77499910295425dd88aad
子沐呀 <163636894+zimuya4153@users.noreply.github.com>
2025-01-13 04:02:17 +0800

    build: update iListenAttentively

--- packages/i/iListenAttentively/versions/0_2_0.lua
+++
@@ -1,7 +0,0 @@
-local deps = {}
-
-function load(package)
-    for _, dep in ipairs(deps) do
-        package:add("deps", dep)
-    end
-end
--- packages/i/iListenAttentively/xmake.lua
+++ packages/i/iListenAttentively/xmake.lua
@@ -1,21 +1,17 @@
 package("ilistenattentively")
-    add_urls("https://github.com/MiracleForest/iListenAttentively-Release.git")
-    add_versionfiles("versions/versions.txt")
+    set_description("iListenAttentively is a rich and modern LeviLamina Minecraft event library!")
 
-    on_load(function(package)
-        import("core.base.semver")
-        local version = package:version_str()
-        version = string.gsub(version, "%.", "_")
-        try { function()
-            import("versions." .. version).load(package)
-        end, catch { function(e)
-            cprint(
-                "${bright yellow}warning: ${clear}Unknown version: ${bright cyan}"
-                .. version .. "${clear}, please resolve dependencies manually."
-            )
-        end } }
-    end)
+    add_urls("https://github.com/MiracleForest/iListenAttentively-Release.git")
+    add_urls("https://github.com/MiracleForest/iListenAttentively-Release/releases/download/$(version)/SDK.zip")
+    add_versions("0.1.0", "e825dbb88bfc597c2a6be3a49ee7555df5dee42d")
+    add_versions("0.2.0", "fde7876a2b1c83dbfc9bdebc1037c54c9f424a72")
+    add_versions("0.2.1", "0642bfc51b7dba18e73736d7d1c7e3efcd526443")
+    add_versions("0.2.2", "f6dcd82cb247cb62b916ccf4ba847668adc39d83b43917f3da84803bee47d99f")
 
     on_install(function(package)
-        import("package.tools.xmake").install(package)
+        if package:version():le("0.2.1") then
+            import("package.tools.xmake").install(package)
+        else
+            os.cp("*", package:installdir())
+        end
     end)
